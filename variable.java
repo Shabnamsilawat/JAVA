ec20e0b3197b106b2376fafb8dd9888a10cf3a49
Shabnam silawat <shabnamsilawat440@gmail.com>
2025-01-02 14:42:57 +0530

    variable.java

---
+++ variable.java
@@ -0,0 +1,14 @@
+public class variable {
+    public static void main (String [] args) {
+        // Variables
+        int a = 10;
+        int b = 25;
+        int sum = a+b;
+        System.out.println(sum);
+        int diff = b -a;
+        System.out.println(diff);
+        int mul = a*b;
+        System.out.println(mul);
+
+    } 
+}
