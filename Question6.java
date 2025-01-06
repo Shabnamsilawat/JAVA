70c8b24e30d45f3b84021ea8294af0813bc67d38
Shabnam silawat <shabnamsilawat440@gmail.com>
2025-01-06 07:48:23 +0530

    Question6.java

---
+++ Question6.java
@@ -0,0 +1,12 @@
+//Print the table of a number input by the user
+import java.util.*;
+public class Question6 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i<11; i++){
+            System.out.println(i*n);
+        }
+    }
+    
+}
