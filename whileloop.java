8ab5c102dd5768195ccf14a86c04951d57fcb4bf
Shabnam silawat <shabnamsilawat440@gmail.com>
2025-01-06 07:45:52 +0530

    whileloop.java

---
+++ whileloop.java
@@ -0,0 +1,11 @@
+//Example for while loop
+public class whileloop {
+    public static void main(String args []){
+        int i = 0;
+        while(i<11){
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+}
