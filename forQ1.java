2f767e378bb5dd2c45d7fe152164aa6358645e2a
Shabnam silawat <shabnamsilawat440@gmail.com>
2025-01-06 07:00:58 +0530

    foeQ1.java

---
+++ forQ1.java
@@ -0,0 +1,16 @@
+//Print numbers 1 to 10 using for loop
+public class forQ1 {
+    public static void main(String [] args){
+        //counter++ = counter= counter+1
+        for(int counter = 0; counter<11; counter++){
+            System.out.println(counter+" ");
+
+        }
+    }
+    
+}
+/*
+ * we can also write it as 
+ * for(int i = 0; i<10;i++){}
+ * System.out.println(i);
+ */
