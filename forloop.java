cabc19efd53c5764cbee761c44bf6ba5f7140a55
Shabnam silawat <shabnamsilawat440@gmail.com>
2025-01-06 06:59:55 +0530

    forlooop.java

---
+++ forloop.java
@@ -0,0 +1,9 @@
+//Example for for loop
+public class forloop {
+    public static void main (String args []){
+        for(int counter = 0; counter <100; counter = counter+1){
+            System.out.println("Hello world");
+        }
+    }
+    
+}
