dbabcf1c3c37f03c95dd39ba0b92618fbd66e6d1
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-01-22 06:39:51 +0530

    While loop

---
+++ Practice15.java
@@ -0,0 +1,11 @@
+//while loop
+import java.util.*;
+public class Practice15 {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i<11){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
