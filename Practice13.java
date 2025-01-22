9683a276482234fe732e8594fdeafb5393c26aa4
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-01-22 06:26:38 +0530

    Add files via upload
    For loop

---
+++ Practice13.java
@@ -0,0 +1,10 @@
+//forloop
+import java.util.*;
+public class Practice13 {
+    public static void main(String[] args) {
+        for(int i=0; i<=11; i++){
+            System.out.println("Hello");
+        }
+    }
+    
+}
