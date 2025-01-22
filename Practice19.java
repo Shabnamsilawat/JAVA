0300a70d9a9a544b8703e168ce046e831ba6d54c
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-01-22 07:17:49 +0530

    Practice 19

---
+++ Practice19.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Practice19 {
+public static void main(String[] args) {
+    int n = 4;
+    int m = 5;
+    for(int i = 0; i <=4; i++){
+        for( int j = 0; j<=5; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+    
+}
