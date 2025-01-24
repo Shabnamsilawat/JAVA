c8797263a2d6f964015dbafc680af1662306564b
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-01-24 12:39:52 +0530

    Inverted half pyramid with numbers

---
+++ Invertedhalfpyramidwithnumbers.java
@@ -0,0 +1,13 @@
+//Print inverted half pyramid with numbers
+import java.util.*;
+public class Invertedhalfpyramidwithnumbers {
+public static void main(String[] args) {
+    int n=5;
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=n-i+1;j++){
+        System.out.print(j);
+    }
+    System.out.println();
+}
+}
+}
