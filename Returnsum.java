8477212e6e932f8ef91f56ddc6e05fe1042301a0
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-01-26 16:53:23 +0530

    Add two numbers and return a sum

---
+++ Returnsum.java
@@ -0,0 +1,16 @@
+//Make a function to add 2 number and then return the sum.
+import java.util.*;
+public class Returnsum {
+    public static int calculateSum(int a, int b){
+        int sum = a+b;
+        return sum;
+    }
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = calculateSum(a,b);
+        System.out.println(sum);
+    }
+    
+}
