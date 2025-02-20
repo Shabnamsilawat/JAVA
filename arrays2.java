be417a57dfafc9767301e32bfebd277c46dcfd34
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-02-20 17:16:57 +0530

    Array example 2

---
+++ arrays2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class arrays2 {
+    public static void main(String args[]){
+        int marks [] = new int [3];
+        marks[0]=95;
+        marks[1]=97;
+        marks[2]=98;
+        for(int i=0; i<3; i++){
+            System.out.println(marks[i]);5
+        }
+    }
+}
