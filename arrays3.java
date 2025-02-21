57c697c1ceaa10d02f1e471f6e6ec2c52f72bc42
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-02-21 19:54:28 +0530

    Array example 3

---
+++ arrays3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class arrays3 {
+    public static void main(String arg[]){
+        int marks[]={95,97,98};
+        for(int i=0; i<3; i++){
+            System.out.println(marks[i]);
+        }
+    }
+    
+}
