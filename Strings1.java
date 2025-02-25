fc0414408839539a60b673215203aa5586c26779
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-02-25 17:34:14 +0530

    Strings example 1

---
+++ Strings1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Strings1 {
+    public static void main (String args []){
+        String name = "Shabnam";
+        String fullname ="Shabnam silawat";
+        String sentence = "My name is shabnam silawat";
+        System.out.println("Your name is:" + name);
+        System.out.println("Your full name is:" +fullname);
+        System.out.println("What is your name?" + sentence);
+    }
+    
+}
