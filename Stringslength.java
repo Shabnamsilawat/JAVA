1881b003c26e99c7015fc0c2374d161bcda08904
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-02-25 18:07:37 +0530

    How to find string length

---
+++ Stringslength.java
@@ -0,0 +1,11 @@
+import java.nio.channels.Pipe.SourceChannel;
+import java.util.*;
+public class Stringslength {
+    public static void main (String args[]){
+        String firstname ="Shabnam";
+        String lastname ="Silawat";
+        String fullname = firstname + " " + lastname;
+        System.out.println(fullname.length());
+    }
+    
+}
