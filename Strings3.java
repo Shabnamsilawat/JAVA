93fd2c171f5abcb0ea04f2a9574a0446d6070ff8
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-02-25 17:51:29 +0530

    String 3 example

---
+++ Strings3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Strings3 {
+    public static void main (String args[]){
+        String firstname = "Shabnam";
+        String lastname = "Silawat";
+        String fullname = firstname + lastname;
+        System.out.println(fullname);
+    }
+    
+}
