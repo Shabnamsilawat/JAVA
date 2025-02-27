6bd78a517f1b703fba476d662cc3b6722ae3b9cf
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-02-27 17:24:33 +0530

    String builder example 4

---
+++ StringBuilders4.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class StringBuilders4 {
+    public static void main (String args[]){
+        StringBuilder sb = new StringBuilder("H");
+        sb.append("e");
+        sb.append("l");
+        sb.append("l");
+        sb.append("o");
+        System.out.println(sb);
+    }
+    
+}
