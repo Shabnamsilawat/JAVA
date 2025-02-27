75a31df8658e249a0640efa5dd482753225a75b1
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-02-27 15:55:45 +0530

    String builder example 2

---
+++ StringsBuilder2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class StringsBuilder2 {
+    public static void main (String args[]){
+        StringBuilder sb = new StringBuilder("Shabnam");
+        System.out.println(sb);
+        sb.insert(0, 'S');
+        System.out.println(sb);
+    }
+    
+}
