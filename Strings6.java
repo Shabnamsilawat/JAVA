013dd7cae935966057e49ef6345407d968b06138
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-02-27 12:37:25 +0530

    Strings example 6

---
+++ Strings6.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Strings6 {
+    public static void main (String args[]){
+        String name1 = "Shabnam";
+        String name2 = "Silawat";
+        if(name1.compareTo(name2)==0){
+            System.out.println("The strings are equal");
+        } else{ 
+            System.out.println("The strings are not equal");
+
+        }
+
+    }
+    
+}
