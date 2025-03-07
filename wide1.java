899cc3bf8e444267994e0f99d6163d3bc0ee8ba6
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-03-07 12:24:11 +0530

    Wide casting or down casting example 1

---
+++ wide1.java
@@ -0,0 +1,11 @@
+public class wide1 {
+    public static void main(String args[]){
+        int a = 8;
+        long b = a;
+        double c = b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+    
+}
