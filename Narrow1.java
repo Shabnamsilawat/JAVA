391dfd789de5925bd20e530c9230ea8623095047
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-03-07 13:10:07 +0530

    Narrowing or upcasting

---
+++ Narrow1.java
@@ -0,0 +1,11 @@
+public class Narrow1 {
+    public static void main (String args[]){
+        double a = 88.8;
+        long b = (long)a;
+        int c = (int)b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+    
+}
