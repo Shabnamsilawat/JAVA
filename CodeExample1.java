e1681db9c0f0fd2f2f45c015f5214e944f18d2f8
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-03-11 12:12:54 +0530

    Code example 1

---
+++ CodeExample1.java
@@ -0,0 +1,15 @@
+public class CodeExample1 {
+    public static void main (String args[]){
+        int var1;
+        int var2;
+        var1 = 50;
+        var2 = 100;
+        System.out.println("Variable 1 is: "+var1);
+        System.out.println("Variable 2 is: "+var2);
+        var1+=var2;
+        var2/=2;
+        System.out.println("Variable 1 is: "+ var1);
+        System.out.println("Variable 2 is: "+ var2);
+    }
+    
+}
