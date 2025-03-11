7c2478f0c0bb102adf259480b3eea85f057dd346
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-03-11 15:32:58 +0530

    If elsse example 2

---
+++ IFELSE3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class IFELSE3 {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        if(num>0){
+            System.out.println("The number is positive");
+        }else if (num<0){
+            System.out.println("The number is negative");
+        }else{
+            System.out.println("The number is zero");
+        }
+        sc.close();
+        }
+    }
+    
