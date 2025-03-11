d11701427a3c51b32576d928236903f99552b6ed
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-03-11 14:07:41 +0530

    If statement example 1

---
+++ IF1.java
@@ -0,0 +1,12 @@
+public class IF1 {
+    public static void main (String args[]){
+        int a = 10;
+        if(a==10){
+            System.out.println("a=10");
+        }
+        if(a>10){
+            System.out.println("a>b");
+        }
+    }
+    
+}
