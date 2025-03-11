0f5320c116952f67457902bf38d58ca5321a0506
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-03-11 14:21:57 +0530

    Else example 1

---
+++ ELSE1.java
@@ -0,0 +1,11 @@
+public class ELSE1 {
+    public static void main(String args[]){
+        int a = 10;
+        if(a == 5){
+            System.out.println("a = 5");
+        }else{
+            System.out.println("a!=5");
+        }
+    }
+    
+}
