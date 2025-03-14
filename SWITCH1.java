3b0e877e89a44f005ad8c12e55e7364edbe7d14a
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-03-14 14:12:15 +0530

    Switch example 1

---
+++ SWITCH1.java
@@ -0,0 +1,18 @@
+public class SWITCH1 {
+    public static void main (String args[]){
+        int i = 10;
+        switch (i){
+            case 10:
+            System.out.println("Ten");
+            break;
+            case 20:
+            System.out.println("Twenty");
+            break;
+            case 30:
+            System.out.println("Thirty");
+            break;
+            default:
+            System.out.println("Invalid input");
+        }
+    }    
+}
