c93642b60280aca70ffcd256bbb6ddf65584954c
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-03-17 12:29:17 +0530

    Conditional operator example 1

---
+++ conditionalop.java
@@ -0,0 +1,13 @@
+public class conditionalop {
+    public static void main (String args[]){
+        int a =7;
+        if(a>5 &&a<10){
+        }
+        if(a==5||a==10){
+        }
+        if(a==3||a==7){  
+        }
+        if(!(a==7)){
+        }
+    }
+}
