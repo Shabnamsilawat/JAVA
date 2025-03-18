277444e97a50d52d46e3b7e0d45bcb2783ba491d
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-03-18 11:09:33 +0530

    For loop example 2

---
+++ forloopex2.java
@@ -0,0 +1,9 @@
+public class forloopex2 {
+    public static void main (String args[]){
+        int n = 20;
+        for(int i=2; i<=n; i+=2){
+            System.out.println(i);
+        }
+    }
+    
+}
