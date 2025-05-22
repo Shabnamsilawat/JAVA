5471d20ca86473ed781d9b0fc1d2201b37c2ba3c
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-05-22 08:50:10 +0530

    Function example 5

---
+++ fn5.java
@@ -0,0 +1,10 @@
+public class fn5 {
+    public static int square(int a){
+        return a*a;
+    }
+    public static void main(String args[]){
+        int result = square(4);
+        System.out.println(result);
+    }
+    
+}
