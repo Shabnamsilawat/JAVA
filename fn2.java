34280265f7c78fe4360efddcd3dbb5f2308a8ca0
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-05-22 08:49:10 +0530

    Function example 2

---
+++ fn2.java
@@ -0,0 +1,9 @@
+public class fn2 {
+    public static int MultiplyByTwo(int value){
+        return value*2;
+    }
+    public static void main(String args[]){
+        int result = MultiplyByTwo(2);
+        System.out.println(result);
+    }
+}
