9221841d9ef4acc04cee7c68010088cb00ff57f2
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-05-22 08:50:32 +0530

    Function example 6

---
+++ fn6.java
@@ -0,0 +1,9 @@
+public class fn6 {
+    public static boolean isEven(int num){
+        return num%2==0;
+    }
+    public static void main(String args[]){
+    boolean result = isEven(4);
+    System.out.println(result);
+    }
+}
