b6b54016aff77f7b533c6d3e9030c3d6e561e550
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-05-22 08:48:53 +0530

    Function example 1

---
+++ fn1.java
@@ -0,0 +1,9 @@
+public class fn1 {
+    public static void printMessage(){
+        System.out.println("First example of function");
+    }
+    public static void main(String args[]){
+        printMessage();
+    }
+    
+}
