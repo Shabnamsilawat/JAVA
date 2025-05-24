55fac67326eeff0ce50d033d760304a3d0e25860
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-05-24 09:59:10 +0530

    Array example 3

---
+++ Array3.java
@@ -0,0 +1,11 @@
+public class Array3 {
+    public static void main(String args[]){
+        int [] array = {1,2,3,4,5};
+        int index =0;
+        for(int currentElement:array){
+            System.out.println("Element at "+ index+ " is: "+ currentElement);
+            index++;
+        }
+    }
+    
+}
