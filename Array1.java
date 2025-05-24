afad3e826535803027941bb4f60eba9964c1c291
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-05-24 09:58:35 +0530

    Array example 1

---
+++ Array1.java
@@ -0,0 +1,12 @@
+public class Array1 {
+    public static void main(String args[]){
+        int [] exampleArray={1,2,3,4,5};
+        int [] exampleArray2 = new int[5];
+        exampleArray[0]=5;
+        exampleArray[4]=1;
+        System.out.println("The number at index 0 is:"+ exampleArray[0]);
+        System.out.println("The number at index 4 is:"+ exampleArray[4]);
+        System.out.println("THe number at index 0 of empty array is:"+ exampleArray2[0]);
+    }
+    
+}
