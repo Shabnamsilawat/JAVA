ec7d218b580f4bbbf58fc9004c4821d5678377f8
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-05-24 09:58:52 +0530

    Array example 2

---
+++ Array2.java
@@ -0,0 +1,10 @@
+public class Array2 {
+    public static void main(String args[]){
+        int [] array = {1,2,3,4,5};
+        int arraySize= array.length;
+        for(int i =0; i<arraySize;i++){
+            System.out.println("Elemenr at index "+i+ " is: "+array[i]);
+        }
+    }
+    
+}
