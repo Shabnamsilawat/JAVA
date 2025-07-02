dc52080495dc588497aa03b9a104ce54e54d7f7e
Shabnamsilawat <130530592+Shabnamsilawat@users.noreply.github.com>
2025-07-02 08:48:39 +0530

    Add files via upload

---
+++ BookCopy.java
@@ -0,0 +1,8 @@
+public class BookCopy {
+    private Book book;
+    private Library library;
+    public BookCopy (Book book, Library library){
+        this.book = book;
+        this.library = library;
+    }
+}
